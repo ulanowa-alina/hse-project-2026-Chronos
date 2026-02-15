f97523b829a6b307bcbb5d87c52af58077831549
Замира Гилязова <zamiragilazova@MacBook-Air-uzivatela-Zamira.local>
2026-02-15 15:56:47 +0300

    edit '\n' in files

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,22 +1,23 @@
 #include <QApplication>
-#include <QWidget>
 #include <QLabel>
 #include <QVBoxLayout>
-#include <pqxx/pqxx>
+#include <QWidget>
 #include <iostream>
+#include <pqxx/pqxx>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
     QWidget window;
     window.setWindowTitle("Chronos");
     window.resize(400, 200);
 
-    QVBoxLayout *layout = new QVBoxLayout(&window);
+    QVBoxLayout* layout = new QVBoxLayout(&window);
 
-    pqxx::connection c("host=localhost port=5432 user=postgres password=mysecretpassword dbname=chronos_db");
+    pqxx::connection c(
+        "host=localhost port=5432 user=postgres password=mysecretpassword dbname=chronos_db");
 
-    QLabel *labelTitle = new QLabel("<h2>Hello World!</h2>");
+    QLabel* labelTitle = new QLabel("<h2>Hello World!</h2>");
 
     layout->addWidget(labelTitle);
 
