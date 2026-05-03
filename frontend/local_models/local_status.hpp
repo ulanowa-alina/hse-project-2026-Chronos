76cdd25b858f12b6a1c5827b465a12e4cfc43bdb
Алина Уланова <alinaulanova@MacBook-Air-Alina-7.local>
2026-05-03 21:29:33 +0300

    added local repository for status

--- frontend/local_models/local_status.hpp
+++ frontend/local_models/local_status.hpp
@@ -1,8 +1,26 @@
-//
-// Created by Алина Уланова on 02.05.2026.
-//
+#ifndef LOCAL_STATUS_HPP
+#define LOCAL_STATUS_HPP
 
-#ifndef CHRONOSCLIENT_LOCAL_STATUS_HPP
-#define CHRONOSCLIENT_LOCAL_STATUS_HPP
+#include <QString>
 
-#endif // CHRONOSCLIENT_LOCAL_STATUS_HPP
+struct LocalStatus {
+    int id_;
+    int board_id_;
+    QString name_;
+    int position_;
+    int is_sync_;
+    int is_deleted_;
+
+    LocalStatus() = default;
+
+    LocalStatus(int id, int board_id, const QString& name, int position, int is_sync,
+                int is_deleted)
+        : id_(id)
+        , board_id_(board_id)
+        , name_(name)
+        , position_(position)
+        , is_sync_(is_sync)
+        , is_deleted_(is_deleted) {
+    }
+};
+#endif // LOCAL_STATUS_HPP
